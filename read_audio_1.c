d91bb2590794d0992bed61b08ca939c1c4970292
PalakMittal <30591092+PalakMittal@users.noreply.github.com>
2021-05-14 14:52:56 +0100

    Add files via upload

---
+++ read_audio_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+ 
+#define N 882
+ 
+int audio_samples ()
+{
+    // Create a 20 ms audio buffer (assuming Fs = 44.1 kHz)
+    int16_t buf[N] = {0}; // buffer
+    int n;                // buffer index
+     
+    // Open WAV file with FFmpeg and read raw samples via the pipe.
+    FILE *pipein;
+    pipein = popen("ffmpeg -i test.wav -f s16le -ac 1 -", "r");
+    fread(buf, 2, N, pipein);
+    pclose(pipein);
+     
+    // Print the sample values in the buffer to a CSV file
+    FILE *csvfile;
+    csvfile = fopen("samples.csv", "w");
+    for (n=0 ; n<N ; ++n) fprintf(csvfile, "%d\n", buf[n]);
+    fclose(csvfile);
+}
+
