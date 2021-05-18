886626b53a25d6ff7ece0debf54a5f5df4081397
PalakMittal <30591092+PalakMittal@users.noreply.github.com>
2021-05-18 11:10:54 +0100

    read_audio_1.c

--- read_audio_1.c
+++ read_audio_1.c
@@ -2,17 +2,16 @@
 #include <stdint.h>
  
 #define N 882
- 
-int audio_samples ()
+// to complile this code: gcc read_audio_1.c -o read_audio_1
+void main()
 {
     // Create a 20 ms audio buffer (assuming Fs = 44.1 kHz)
     int16_t buf[N] = {0}; // buffer
     int n;                // buffer index
      
     // Open WAV file with FFmpeg and read raw samples via the pipe.
- // to allow this part of the code to work you need to [ sudo apt-get install ffmpeg ] 
     FILE *pipein;
-    pipein = popen("ffmpeg -i test.wav -f s16le -ac 1 -", "r");
+    pipein = popen("ffmpeg -i starwars.wav -f s16le -ac 1 -", "r");
     fread(buf, 2, N, pipein);
     pclose(pipein);
      
