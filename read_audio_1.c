e0eef30d8e4415f2ca8b33cce9b56e30b74b484d
PalakMittal <30591092+PalakMittal@users.noreply.github.com>
2021-05-21 14:15:49 +0100

    Update read_audio.c

--- read_audio_1.c
+++ read_audio_1.c
@@ -9,13 +9,15 @@
 #include <stdio.h>
 #include <stdint.h>
  
-#define N 882
+//to avoid buffer overflow N = 44100* seconds
+ 
+#define N 2646000
  
 void main()
 {
     // Create a 20 ms audio buffer (assuming Fs = 44.1 kHz)
     int16_t buf[N] = {0}; // buffer
-    // used a fixed length int data type int16_t to make sure that I got 16 bit signed itnerger samples
+    // used a fixed length int data type int16_t to make sure that I got 8 bit signed itnerger samples
     //this type is defined in the stdint.h header file
     
     
@@ -24,11 +26,10 @@ void main()
     // Open WAV file with FFmpeg and read raw samples via the pipe.
     FILE *pipein;
     
-    pipein = popen("ffmpeg starwars.wav -f s16le -ac 1 -i -", "r");
+    pipein = popen("ffmpeg -i test.wav -f s16le -ac 1 -", "r");
     // popen() fucntion lauches a seperate program, which is accessible via the pipe
     // the first argumnet is the command line for the program being launched [ffmepg] 
-    // the secons arguemnt 'r' means that we'll be reading data from that program 
-    
+    // the secons arguemnt 'r' means that we'll be reading data from that program
     fread(buf, 2, N, pipein);
     pclose(pipein);
     //close the pipe
@@ -39,13 +40,9 @@ void main()
     for (n=0 ; n<N ; ++n) fprintf(csvfile, "%d\n", buf[n]);
     fclose(csvfile);
     
-    
-    
-    
     // ffmpeg -i starwars.wav -f s16le -ac 1 -"
     // -i ------- read data from the standard input in this case the pipe
     // -f s16le ----- tells ffmepg that the fromat of the audio data it reads is raw PCM( pulse code modulation, is to reprsnet the sampled analog signals digitally ), sigend integer, 16 bit and little endian 
     // -ac 1 -------- the number of channes in the audio data it is reading is one  
-}
 }
 
