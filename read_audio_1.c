e82536afd70eb05debd4196e1de0e1de3faf5cc9
PalakMittal <30591092+PalakMittal@users.noreply.github.com>
2021-05-22 16:59:19 +0100

    Update read_audio_1.c
    To avoid buffer overflow with the sample values: 44100 x duration of audio in seconds [ 44100 is the frequency we are working with]

--- read_audio_1.c
+++ read_audio_1.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <stdint.h>
  
-//to avoid buffer overflow N = 44100* seconds
+//to avoid buffer overflow N = 44100* seconds [duration of the audio] 
  
 #define N 2646000
  
@@ -22,27 +22,43 @@ void main()
     
     
     int n;                // buffer index
-     
+    //int max_name = 20; 
+    	char name_of_file[50];
+  	printf("Name of audio file: ");
+  	scanf("%s", name_of_file);
+  	
+  	// getting the name of the audio file, but has to be in the same directory as the code
+
+  	char buffer[100];
+  	sprintf(buffer, "ffmpeg -i %s -f s16le -ac 1 -", name_of_file);
+	printf("%s", buffer);
+    
+    
     // Open WAV file with FFmpeg and read raw samples via the pipe.
     FILE *pipein;
-    
-    pipein = popen("ffmpeg -i test.wav -f s16le -ac 1 -", "r");
+       
+    pipein = popen(buffer, "r");
     // popen() fucntion lauches a seperate program, which is accessible via the pipe
     // the first argumnet is the command line for the program being launched [ffmepg] 
     // the secons arguemnt 'r' means that we'll be reading data from that program
     fread(buf, 2, N, pipein);
     pclose(pipein);
     //close the pipe
-     
+    //for (n=0 ; n<N ; ++n) 
+   //	printf("%d\n", buf[n]);
     // Print the sample values in the buffer to a CSV file
+    
+    
     FILE *csvfile;
     csvfile = fopen("samples.csv", "w");
-    for (n=0 ; n<N ; ++n) fprintf(csvfile, "%d\n", buf[n]);
+    for (n=0 ; n<N ; ++n) 
+    	fprintf(csvfile, "%d\n", buf[n]);
     fclose(csvfile);
     
     // ffmpeg -i starwars.wav -f s16le -ac 1 -"
     // -i ------- read data from the standard input in this case the pipe
     // -f s16le ----- tells ffmepg that the fromat of the audio data it reads is raw PCM( pulse code modulation, is to reprsnet the sampled analog signals digitally ), sigend integer, 16 bit and little endian 
     // -ac 1 -------- the number of channes in the audio data it is reading is one  
+ 
+    
 }
-
