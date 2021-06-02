5d4d44051cc21031abcf9b3e2d6ea49cb2b26c5c
ATinyFish3 <60041166+ATinyFish3@users.noreply.github.com>
2021-06-03 00:15:18 +0100

    Add files via upload

--- textSteg.c
+++ textSteg.c
@@ -5,7 +5,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
 #include <limits.h>
 #include <ctype.h>
@@ -13,15 +12,16 @@
 int encodeText()
 {
 	
-	int random;//variable that gets set to a random number in order to pick a character from a random position in characters[]
-	srand(time(NULL));//seed rand() with time so rand() is random/different for each run of the program
 	
 	int i = 0;
 	int j = 0;
 	
 	FILE *fileInp;//declare file pointer
-	char textInp[500];//create character variable of 100 bytes to write the contents of the file to
-	fileInp = fopen("text","r");//open file called text to read from
+	char textInp[500];//create character variable of 500 bytes to write the contents of the file to
+	char filename[50];
+	printf("Enter the name of the file you want to read from\n");
+	scanf("%s",filename);
+	fileInp = fopen(filename,"r");//open file to read from
    	if(fileInp == NULL) {//checks if the file exists
       		perror("Error opening file");//if file doesn't exist then print error message
       		return(-1);//program exit due to error
@@ -29,8 +29,7 @@ int encodeText()
    	fgets(textInp,500,(FILE*)fileInp);//copy contents of file to textInp variable
 	fclose(fileInp);
 
-	//printf("%s\n",textInp);
-	
+
 	for(i = 0; textInp[i] != '\0'; ++i){//iterate through textInp
 		while (!( (textInp[i] >= 'a' && textInp[i] <= 'z') || (textInp[i] >= 'A' && textInp[i] <= 'Z') || (textInp[i] >= '0' && textInp[i] <= '9') || textInp[i] == '\0') ){//remove characters that are not alphanumeric
 			for(j = i; textInp[j] != '\0'; ++j){
@@ -47,10 +46,6 @@ int encodeText()
 		}
 	}
 	
-	//puts(textInp);
-	
-	//how vexingly quick 9761240835 daft zebras jump - pangram
-	//the jay, pig, fox, zebra and my [num] wolves quack
 	
 	char container[] = "how vexingly quick 9761240835 daft zebras jump";//pangram (containers all alphanumeric characters to check against the input text with 
 	char positions[1000] = "";//will store the positions of each character from the input text (1000 as textInp = 500)
@@ -72,11 +67,97 @@ int encodeText()
 	}
 	
 	printf("%s. the jay, pig, fox, zebra and my %s wolves quack\n",container, positions);
+	
+	FILE *fileOut;//declare file pointer
+	printf("Enter a name for the file where your encoded message will be saved\n");
+	scanf("%s",filename);
+	fileOut = fopen(filename,"w");//open file to write to
+	//write output to file
+   	fputs(container, fileOut);
+   	fputs(". the jay, pig, fox, zebra and my ",fileOut);
+   	fputs(positions, fileOut);
+   	fputs(positions,fileOut);
+   	fputs(" wolves quack\n",fileOut);
+	fclose(fileInp);
+	
 	return(0);
+}
+
+int decodeText()
+{
+
+	FILE *fileInp;//declare file pointer
+	char encoded[1093];//create character variable of 10393 (max size of final message) bytes to write the contents of the file to
+	char filename[50];
+	printf("Enter the name of the file you want to read from\n");
+	scanf("%s",filename);
+	fileInp = fopen(filename,"r");//open file to read from
+   	if(fileInp == NULL) {//checks if the file exists
+      		perror("Error opening file");//if file doesn't exist then print error message
+      		return(-1);//program exit due to error
+   	}
+   	fgets(encoded,1093,(FILE*)fileInp);//copy contents of file to textInp variable
+	fclose(fileInp);
+	
+	char container[] = "how vexingly quick 9761240835 daft zebras jump";
+	char positions[1000]="";//variable to store the long string of digits from the encoded message
+	char message[500] = "";//variable that will hold the decoded message
+
+	int i=80;//position of the first digit in the position strings (find better way of doing this as poor maintainability)
+	
+	char digit[]="p";
+	while(encoded[i]!=' '){//iterate through encoded until the end of the string of digits (' ')
+		digit[0]=encoded[i];//get value of encoded[i] that can be used with strcat()
+		strcat(positions,digit);//append the character to positions
+		//puts(positions);//for testing
+		i++;//increment to go to next character in encoded
+	}
+	
+	//for loop atoi on each pair of digits and compare with container
+
+	int num;//integer to store the value retreived from 'positions'
+	char toJoin[]="tj";//place holder string to enable use of atoi on numbers in a string that can be 1 or 2 digits long
+	char character[]="c";//placeholder char to enable strcat()
+	for(i=0;i<strlen(positions);i=i+2){//iterate through the pairs of digits in 'positions'
+		toJoin[0]=positions[i];
+		toJoin[1]=positions[i+1];
+		num = atoi(toJoin);//convert number in string to integer
+		character[0]=container[num];
+		strcat(message,character);//append current character to message string
+	}
+	
+	puts(message);
+	
+	FILE *fileOut;//declare file pointer
+	printf("Enter a name for the file where your encoded message will be saved\n");
+	scanf("%s",filename);
+	fileOut = fopen(filename,"w");//open file to write to
+	//write output to file
+   	fputs(message, fileOut);
+	fclose(fileInp);
+
+	return(0);	
 }
 
 int main()
 {
-	encodeText();
+
+	printf("Do you want to (E)ncode or (D)ecode?\n");//ask user for input of E or D
+	char c;//char to store input result
+	//while loop to ensure that the only input is E or D; all other input ignored
+	while((c!='E')&&(c!= 'D')){
+		c =getchar();
+	}
+	if(c=='E'){
+		encodeText();
+	}
+	else if(c=='D'){
+		decodeText();
+	}
+	else{
+		printf("Error");
+		return(-1);
+	}
+	
 	return(0);
 }
