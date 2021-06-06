89b5a7c21fa49e50b13184712a1f6867819db6cb
nathanc18 <79257670+nathanc18@users.noreply.github.com>
2021-06-06 15:17:34 +0100

    Update Encrpyt.cpp

--- Encrpyt.cpp
+++ Encrpyt.cpp
@@ -1,55 +1,58 @@
 // Encrpyt.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+
+
+// Instructions - Input file must be in the same folder as program, and the output file will be saved to the same location. File must have format of .txt
 #pragma warning(disable : 4996)
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <uchar.h>
+#include <stdlib.h>                                             // supports memory managment
+#include <string.h>                                             // support string manipulation
+#include <uchar.h>                                              // supports encrpytion
 #define MAX 256
-FILE* fp;
-FILE* fp2;
-char line[MAX];
-char tgt[MAX];
-char file[256], * end;
-int hex_to_binary(char*);
+FILE* fp;                                                       // point to input file
+FILE* fp2;                                                      // point to output file
+char line[MAX];                                                 // define array with maximun space
+char tgt[MAX];                                                  // define array with maximun space
+char file[256], * end;                                          // array to read file letter by letter and point to the end
+int hex_to_binary(char*);                                        
 
 int main() {
 
-    printf("Enter the name of the file: ");
-    fgets(file, 256, stdin);
+    printf("Enter the name of the .txt file: ");                         // enter name of file to be opened and read
+    fgets(file, 256, stdin);                                        // *file must be in same folder as program folder (visual basic)
 
-    printf("Enter your output file name:");
-    scanf("%s", tgt);
+    printf("Enter your output file name:");                         // enter name of file to be created with encrypted text
+    scanf("%s", tgt);                                               // file will be saved in the same folder where the intiial text file must be
 
-    if ((end = strchr(file, '\n')) != NULL)
+    if ((end = strchr(file, '\n')) != NULL)                         // points to the first character in the file. The end of the file is null 
         *end = '\0';
-    FILE* fp = fopen(file, "r");
+    FILE* fp = fopen(file, "r");                                    // points to the input file and opens it for reading
     //fprintf(fp, "test123");   testing to see if file is opened
  
-    fp2 = fopen(tgt, "wt");
+    fp2 = fopen(tgt, "wt");                                         // open second file (output file) for writing
    
     int c;
    
-        fp = fopen(file,"rb");
+        fp = fopen(file,"rb");                                      // opening file to read binary 
         if (fp)
         {
             do
             {
-                c = fgetc(fp);
-                if (EOF != c)
-                    fprintf(fp2,"%02X ",c);             
-            } while (EOF != c);
+                c = fgetc(fp);                                      // c is equal to the the next character in the first file
+                if (EOF != c)                                       //  if it is not the end of the file print the current c
+                    fprintf(fp2,"%02X ",c);                         // print in hex first to target file 
+            } while (EOF != c);                                     // loop whilst it is not the end  of the file
             
         }
-    fclose(fp);
-    fclose(fp2);
+    fclose(fp);                                                     // close input file
+    fclose(fp2);                                                    // close output file
 
-    fp = fopen(tgt, "r");
+    fp = fopen(tgt, "r");                                           // open input file with tgt array 
    
 
-    while (!feof(fp)) {
-        fgets(line, MAX, fp);
-        hex_to_binary(line);
+    while (!feof(fp)) {                                             // while it is not the end of the file
+        fgets(line, MAX, fp);                                       // read all lines of the text file and save to array
+        hex_to_binary(line);                                        //  send line array to hex to binary function                                    
 
     }
   
@@ -59,25 +62,25 @@ int hex_to_binary(char* hex_string)
 {
 
 
-    int i = 0;
-    fp2 = fopen(tgt, "wt");
-    while (hex_string[i])
+    int i = 0;                                                       // character counter to ensure 8 bit encryption 
+    fp2 = fopen(tgt, "wt");                                          // open target file for writing 
+    while (hex_string[i])                                            // counts the string in hex in relation to i
     {
 
-        switch (hex_string[i])
+        switch (hex_string[i])                                     
         {
-        case '0':
+        case '0':                                                    // if character is 0
                  {
-                     if (i % 2 == 0)
+                     if (i % 2 == 0)                                 // checking the character count to ensure 8 bit encription so the last bit in the 8bit word is changed
                                  fprintf(fp2, "0001");
                      else
                                  fprintf(fp2, "0000");
                  }
          break;
 
-        case '1':
+        case '1':                                                    
                 {
-                if (i % 2 == 0)
+                if (i % 2 == 0)                                       
                     fprintf(fp2, "0000");
                 else
                     fprintf(fp2, "0001");
@@ -246,22 +249,8 @@ int hex_to_binary(char* hex_string)
             }
             break;
     }
-            printf("%c", hex_string[i]);    
-            i++;
+            printf("%c", hex_string[i]);                                // print the binary character 
+            i++;                                                        // increase i by 1
         }
         return 0;
     }
-  
-    
-   
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
-   
